fdacd3e98d93e9efbae8e97ad26e7f3574e6a17d
J27mendez <mendes_j27@hotmail.com>
2024-06-27 12:48:39 -0300

    chore: add pg and dotenv setup, src folder creation and .env settings

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import { PostgresHelper } from './src/db/podtgres/helper.js'
+import express from 'express'
+import dotenv from 'dotenv'
 
+const app = express()
+dotenv.config()
+
+app.get('/', async (req, res) => {
+  try {
+    const results = await PostgresHelper.query('SELECT * FROM users;')
+    res.json(results.rows)
+  } catch (err) {
+    console.error('erro ao executar consulta:', err)
+    res.status(500).json({ error: 'erro ao buscar dados no banco de dados' })
+  }
+})
+
+app.listen(3000, () => console.log('listening on port 3000'))
---
+++ src/db/postgres/helper.js
@@ -0,0 +1,23 @@
+import pg from 'pg'
+
+const { Pool } = pg
+
+export const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+})
+
+export const PostgresHelper = {
+  query: async (query, params) => {
+    const client = await pool.connect()
+    try {
+      const results = await client.query(query, params)
+      return results.rows
+    } finally {
+      client.release()
+    }
+  },
+}
