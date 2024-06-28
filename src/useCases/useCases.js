2c2aa6b46e344aec44e9a6c684eb7c7709555cdb
J27mendez <mendes_j27@hotmail.com>
2024-06-28 18:06:50 -0300

    feat: add CreateUserUseCase

---
+++ src/useCases/useCases.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcrypt'
+
+import { PostgresCreateUserRepository } from '../repositories/postgres/createUser.js'
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    //TODO: verificar se o email ja esta em uso
+
+    //gerar ID do usuario
+    const userId = uuidv4()
+
+    //criptografar a senha
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
+
+    //inserir o usuario no banco de dados
+    const user = {
+      ...createUserParams,
+      id: userId,
+      password: hashedPassword,
+    }
+    // chamar o repositorio
+    const postgresCreateUserRepository = new PostgresCreateUserRepository()
+
+    const createUser = await postgresCreateUserRepository.execute(user)
+
+    return createUser
+  }
+}
