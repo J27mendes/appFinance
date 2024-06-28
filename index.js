b4370222374aedccc34065bef3a44059824da9b2
J27mendez <mendes_j27@hotmail.com>
2024-06-28 14:50:10 -0300

    feat: add PostgresCreateUserRepository

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-import { PostgresHelper } from './src/db/postgres/helper.js'
 import express from 'express'
 import dotenv from 'dotenv'
 
 const app = express()
+app.use(express.json())
 dotenv.config()
 
-app.get('/', async (req, res) => {
-  try {
-    const results = await PostgresHelper.query('SELECT * FROM users;')
-    res.json(results.rows)
-  } catch (err) {
-    console.error('erro ao executar consulta:', err)
-    res.status(500).json({ error: 'erro ao buscar dados no banco de dados' })
-  }
-})
-
-app.listen(3000, () => console.log('listening on port 3000'))
+app.listen(process.env.PORT, () => console.log('listening on port 3000'))
