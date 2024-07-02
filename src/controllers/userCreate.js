6b7c2cfbaeeb2bfa48bdd4393de193c4ec2837b4
J27mendez <mendes_j27@hotmail.com>
2024-07-02 12:55:05 -0300

    feat: check if the email already exists in the database

--- src/controllers/userCreate.js
+++ src/controllers/userCreate.js
@@ -1,5 +1,7 @@
 import { CreateUserUseCase } from '../useCases/createUser.js'
+import { PostgresCompareEmail } from '../repositories/postgres/compareEmail.js'
 import validator from 'validator'
+import { badRequest, created, serverError } from './helpers.js'
 
 export class CreateUserController {
   async execute(httpRequest) {
@@ -10,52 +12,39 @@ export class CreateUserController {
 
       for (const field of requiredFields) {
         if (!params[field] || params[field].trim().length === 0) {
-          return {
-            statusCode: 400,
-            body: {
-              errorMessage: `Missing param:${field}`,
-            },
-          }
+          return badRequest({ message: `Missing param: ${field}` })
         }
       }
+
       const passwordIsValid = params.password.length < 6
 
       if (passwordIsValid) {
-        return {
-          statusCode: 400,
-          body: {
-            errorMessage: 'Password must be at least 6 characters',
-          },
-        }
+        return badRequest({ message: 'Password must be at least 6 characters' })
+      }
+      // Verificar se o email já está em uso
+      const postgresCompareEmail = new PostgresCompareEmail()
+      const emailExistsResult = await postgresCompareEmail.execute(params.email)
+      const emailExists = emailExistsResult[0].exists
+      if (emailExists) {
+        return badRequest({
+          message: 'Email already in use. Please choose another one.',
+        })
       }
 
       const emailIsValid = validator.isEmail(params.email)
 
       if (!emailIsValid) {
-        return {
-          statusCode: 400,
-          body: {
-            errorMessage: 'Invalid e-mail. Please provide a valid one.',
-          },
-        }
+        return badRequest({
+          message: 'Invalid e-mail. Please provide a valid one.',
+        })
       }
       //chamar o use case
       const createUserUseCase = new CreateUserUseCase()
 
       const createdUser = await createUserUseCase.execute(params)
-      //retornar a resposta para o usuario (status code)
-      return {
-        statusCode: 201,
-        body: createdUser,
-      }
+      return created({ createdUser })
     } catch (error) {
-      console.error(error)
-      return {
-        statusCode: 500,
-        body: {
-          errorMessage: 'Internal server error',
-        },
-      }
+      return serverError(error)
     }
   }
 }
