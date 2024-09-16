5b39cc301858bb477a5d223b0913e4227fc05b75
J27mendez <mendes_j27@hotmail.com>
2024-09-16 20:29:11 -0300

    add zod schemas for validation in userCreate

--- src/controllers/user/userCreate.js
+++ src/controllers/user/userCreate.js
@@ -1,15 +1,7 @@
 import { EmailExistsError } from '../../errors/user.js'
-import {
-  badRequest,
-  created,
-  serverError,
-  checkIfEmailIsValid,
-  checkIfPasswordIsValid,
-  emailIsAlreadyInUseResponse,
-  invalidPasswordResponse,
-  requiredFieldsIsMissingResponse,
-  validateRequestFields,
-} from '../helpers/index.js'
+import { createUserSchema } from '../../schemas/index.js'
+import { badRequest, created, serverError } from '../helpers/index.js'
+import { ZodError } from 'zod'
 export class CreateUserController {
   constructor(createUserUseCase) {
     this.createUserUseCase = createUserUseCase
@@ -18,29 +10,17 @@ export class CreateUserController {
     try {
       const params = httpRequest.body
 
-      const requiredFields = ['first_name', 'last_name', 'email', 'password']
-
-      const { ok: requiredFieldsWereProvided, missingField } =
-        validateRequestFields(params, requiredFields)
-      if (!requiredFieldsWereProvided) {
-        return requiredFieldsIsMissingResponse(missingField)
-      }
-
-      const passwordIsValid = checkIfPasswordIsValid(params.password)
-
-      if (!passwordIsValid) {
-        return invalidPasswordResponse()
-      }
-
-      const emailIsValid = checkIfEmailIsValid(params.email)
-
-      if (!emailIsValid) {
-        return emailIsAlreadyInUseResponse()
-      }
+      await createUserSchema.parseAsync(params)
 
       const createdUser = await this.createUserUseCase.execute(params)
       return created({ createdUser })
     } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest({
+          message: error.errors[0].message,
+        })
+      }
+
       if (error instanceof EmailExistsError) {
         return badRequest({ message: error.message })
       }
