ee25e31c937de79ef80cce7277f2deb0a80f7f3f
J27mendez <mendes_j27@hotmail.com>
2024-12-27 18:22:21 -0300

    test: ensure returns 500 if exists a server error

--- src/controllers/transaction/getTransactionById.test.js
+++ src/controllers/transaction/getTransactionById.test.js
@@ -76,4 +76,18 @@ describe('Get Transaction By User Id Controller', () => {
     //assert
     expect(response.statusCode).toBe(404)
   })
+
+  it('should return 500 when exist a server error', async () => {
+    //arrange
+    const { sut, getUserByIdUseCase } = makeSut()
+    jest.spyOn(getUserByIdUseCase, 'execute').mockRejectedValue(new Error())
+
+    //act
+    const response = await sut.execute({
+      query: { userId: faker.string.uuid() },
+    })
+
+    //assert
+    expect(response.statusCode).toBe(500)
+  })
 })
