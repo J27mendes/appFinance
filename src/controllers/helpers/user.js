011cc81924008a0087df80c90121d9a8c3a2be2a
J27mendez <mendes_j27@hotmail.com>
2025-01-04 17:07:59 -0300

    refactor: remove unused code

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,10 +1,4 @@
-import { badRequest, notFound } from './http.js'
-
-export const invalidPasswordResponse = () =>
-  badRequest({ message: 'Password must be at least 6 characters.' })
-
-export const emailIsAlreadyInUseResponse = () =>
-  badRequest({ message: 'Invalid e-mail. Please provide a valide one.' })
+import { notFound } from './http.js'
 
 export const userNotFoundResponse = (id) =>
   notFound({ message: `User with ${id} not found.` })
--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -1,8 +1,6 @@
 import validator from 'validator'
 import { badRequest } from './http.js'
 
-export const checkIfEmailIsValid = (email) => validator.isEmail(email)
-
 export const checkIfIdIsValid = (id) => validator.isUUID(id)
 
 export const invalidIdResponse = () =>
@@ -12,39 +10,4 @@ export const requiredFieldsIsMissingResponse = (field) => {
   return badRequest({
     message: `The field ${field} is required.`,
   })
-}
-
-export const checkIfIsString = (value) => typeof value === 'string'
-
-export const validateRequestFields = (params, requiredFields) => {
-  for (const field of requiredFields) {
-    const value = params[field]
-
-    const fieldIsMissing = value === undefined || value === null
-
-    let fieldIsEmpty = false
-
-    if (field === 'id' || field === 'user_id') {
-      fieldIsEmpty = !checkIfIdIsValid(value)
-    } else if (typeof value === 'string') {
-      fieldIsEmpty = validator.isEmpty(value, { ignore_whitespace: true })
-    } else if (typeof value === 'number') {
-      fieldIsEmpty = isNaN(value)
-    }
-
-    if (fieldIsMissing || fieldIsEmpty) {
-      console.log(
-        `Field check - field: ${field}, fieldIsMissing: ${fieldIsMissing}, fieldIsEmpty: ${fieldIsEmpty}`,
-      )
-
-      return {
-        missingField: false,
-        ok: false,
-      }
-    }
-  }
-  return {
-    ok: true,
-    missingField: undefined,
-  }
 }
