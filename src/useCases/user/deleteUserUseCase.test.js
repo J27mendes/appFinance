74ea861bc6d55a938fa5ee3d7cdca90754c7f486
J27mendez <mendes_j27@hotmail.com>
2025-01-08 15:50:37 -0300

    test: ensures that parameters are provided correctly

--- src/useCases/user/deleteUserUseCase.test.js
+++ src/useCases/user/deleteUserUseCase.test.js
@@ -35,4 +35,17 @@ describe('DeleteUserUseCase', () => {
     //assert
     expect(deletedUser).toEqual(user)
   })
+
+  it('should call DeleteUserUseCase with correct params', async () => {
+    //arrange
+    const { sut, deleteUserRepository } = makeSut()
+    const executeSpy = jest.spyOn(deleteUserRepository, 'execute')
+    const userId = faker.string.uuid()
+
+    //act
+    await sut.execute(userId)
+
+    //assert
+    expect(executeSpy).toHaveBeenCalledWith(userId)
+  })
 })
