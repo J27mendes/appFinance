c8884400ef90343a24cef8116a05b3bbc49a5fda
J27mendez <mendes_j27@hotmail.com>
2025-01-11 16:39:45 -0300

    test: ensute CreateTransactionRepository throws

--- src/useCases/transaction/createTransaction.test.js
+++ src/useCases/transaction/createTransaction.test.js
@@ -140,4 +140,18 @@ describe('CreateTransactionUseCase', () => {
     //assert
     await expect(promise).rejects.toThrow()
   })
+
+  it('should throw if CreateTransactionRepository throws', async () => {
+    //arrange
+    const { sut, createTransactionRepository } = makeSut()
+    jest
+      .spyOn(createTransactionRepository, 'execute')
+      .mockRejectedValueOnce(new Error())
+
+    //act
+    const promise = sut.execute(transaction)
+
+    //assert
+    await expect(promise).rejects.toThrow()
+  })
 })
