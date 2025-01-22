3b05890473c949afebb19649aa004d6149b4058e
J27mendez <mendes_j27@hotmail.com>
2025-01-22 14:01:46 -0300

    test: ensure PasswordHasherAdapter return password criptograph

---
+++ src/adapters/passwordHasher.test.js
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker'
+import { PasswordHasherAdapter } from './passwordHasher'
+
+describe('PasswordHasherAdapter', () => {
+  it('should return hashed password', async () => {
+    //arrange
+    const sut = new PasswordHasherAdapter()
+    const password = faker.internet.password()
+
+    //act
+    const result = await sut.execute(password)
+
+    //asert
+    expect(result).toBeTruthy()
+    expect(typeof result).toBe('string')
+    expect(result).not.toBe(password)
+  })
+})
