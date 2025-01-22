4d1f64db5c8d7e9a2ee130981534c11af7461952
J27mendez <mendes_j27@hotmail.com>
2025-01-22 13:55:42 -0300

    test: ensure IdGeneratorAdapter returns random id

---
+++ src/adapters/idGeneratorAdapter.test.js
@@ -0,0 +1,15 @@
+import { IdGeneratorAdapter } from './idGenerator'
+
+describe('idGeneratorAdapter', () => {
+  it('should return a random id', async () => {
+    const sut = new IdGeneratorAdapter()
+    const uuidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
+    const result = await sut.execute()
+
+    expect(result).toBeTruthy()
+    expect(typeof result).toBe('string')
+    expect(result).toMatch(uuidRegex)
+  })
+})
