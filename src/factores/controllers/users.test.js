77e869478fec3dd5c401f8880f2344e8c090e053
J27mendez <mendes_j27@hotmail.com>
2025-01-22 13:12:16 -0300

    test: ensure makeGetUserById istanceOf GetUserByIdController

--- src/factores/controllers/users.test.js
+++ src/factores/controllers/users.test.js
@@ -2,16 +2,22 @@ import {
   CreateUserController,
   DeleteUserController,
   GetUserBalanceController,
+  GetUserByIdController,
   UpdateUserController,
 } from '../../controllers'
 import {
   makeDeleteUserById,
   makeGetUserBalanceController,
+  makeGetUserById,
   makePostUser,
   makeUpdateUserById,
 } from './users'
 
 describe('Users Controllers Factories', () => {
+  it('should return a valid GetUserByIdController instance', () => {
+    expect(makeGetUserById()).toBeInstanceOf(GetUserByIdController)
+  })
+
   it('should return a valid CreateUserController instance', () => {
     expect(makePostUser()).toBeInstanceOf(CreateUserController)
   })
