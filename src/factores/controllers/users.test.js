8bc6833b00f6591d9ad964053bd02b58bd6e771f
J27mendez <mendes_j27@hotmail.com>
2025-01-22 13:05:55 -0300

    test: ensure makeUpdateUserById instanceOf UpdateUserController

--- src/factores/controllers/users.test.js
+++ src/factores/controllers/users.test.js
@@ -1,8 +1,12 @@
-import { CreateUserController } from '../../controllers'
-import { makePostUser } from './users'
+import { CreateUserController, UpdateUserController } from '../../controllers'
+import { makePostUser, makeUpdateUserById } from './users'
 
 describe('Users Controllers Factories', () => {
   it('should return a valid CreateUserController instance', () => {
     expect(makePostUser()).toBeInstanceOf(CreateUserController)
+  })
+
+  it('should return a valid UpdateUserController instance', () => {
+    expect(makeUpdateUserById()).toBeInstanceOf(UpdateUserController)
   })
 })
