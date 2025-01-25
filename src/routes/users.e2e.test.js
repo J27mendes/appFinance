a998be7f3bb30aa655024857b9f42efeb626fb4c
J27mendez <mendes_j27@hotmail.com>
2025-01-25 13:06:17 -0300

    test: ensure PATCH /api/users/:userId returns 200 when user is updated

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -1,6 +1,7 @@
 import supertest from 'supertest'
 import { app } from '../app.js'
 import { user } from '../tests/fixtures/index.js'
+import { faker } from '@faker-js/faker'
 
 const request = supertest
 
@@ -28,5 +29,31 @@ describe('User Routers E2E tests', () => {
 
     expect(response.status).toBe(200)
     expect(response.body).toEqual(createdUser)
+  })
+
+  it('PATCH /api/users/:userId should return 200 when user is updated', async () => {
+    const { body: createdUser } = await request(app)
+      .post('/api/users')
+      .send({
+        ...user,
+        id: undefined,
+      })
+
+    const updateUserParams = {
+      first_name: faker.person.firstName(),
+      last_name: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+
+    const response = await request(app)
+      .patch(`/api/users/${createdUser.id}`)
+      .send(updateUserParams)
+
+    expect(response.status).toBe(200)
+    expect(response.body.first_name).toBe(updateUserParams.first_name)
+    expect(response.body.last_name).toBe(updateUserParams.last_name)
+    expect(response.body.email).toBe(updateUserParams.email)
+    expect(response.body.password).not.toBe(updateUserParams.password)
   })
 })
