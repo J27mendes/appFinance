85e2d89c6c42d54bf811c8d0563ec393ff0208a8
J27mendez <mendes_j27@hotmail.com>
2025-01-25 12:47:36 -0300

    test: ensure GET /ApiUsers/userId returns 200 when user is found

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -15,4 +15,18 @@ describe('User Routers E2E tests', () => {
 
     expect(response.status).toBe(201)
   })
+
+  it('GET /users/:userId should return 200 when user is found', async () => {
+    const { body: createdUser } = await request(app)
+      .post('/api/users')
+      .send({
+        ...user,
+        id: undefined,
+      })
+
+    const response = await request(app).get(`/api/users/${createdUser.id}`)
+
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(createdUser)
+  })
 })
