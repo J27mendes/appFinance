d1c95976c195d2afc5b90d9d3f613d9ebcbd8d19
J27mendez <mendes_j27@hotmail.com>
2025-01-25 09:31:53 -0300

    refactor: add paste router with index

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 import express from 'express'
 import 'dotenv/config.js'
-
-import {
-  makeGetTransactionByUserIdController,
-  makeCreateTransactionController,
-  makeUpdateTransactionController,
-  makeDeleteTransactionController,
-} from './src/factores/controllers/transactions.js'
 import { userRouter } from './src/routes/users.js'
+import { transactionsRouter } from './src/routes/transactions.js'
 
 const app = express()
 
@@ -15,39 +9,7 @@ app.use(express.json())
 
 app.use('/api/users', userRouter)
 
-app.get('/api/transactions/:userId', async (request, response) => {
-  const getTransactionsByUserIdController =
-    makeGetTransactionByUserIdController()
-
-  const { statusCode, body } =
-    await getTransactionsByUserIdController.execute(request)
-
-  response.status(statusCode).send(body)
-})
-
-app.post('/api/transactions/:userId', async (request, response) => {
-  const createTransactionController = makeCreateTransactionController()
-  const { statusCode, body } =
-    await createTransactionController.execute(request)
-  response.status(statusCode).send(body)
-})
-
-app.patch('/api/transactions/:transactionId', async (request, response) => {
-  const updateTransactionController = makeUpdateTransactionController()
-
-  const { statusCode, body } =
-    await updateTransactionController.execute(request)
-
-  response.status(statusCode).send(body)
-})
-
-app.delete('/api/transactions/:transactionId', async (request, response) => {
-  const deleteTransactionById = makeDeleteTransactionController()
-
-  const { statusCode, body } = await deleteTransactionById.execute(request)
-
-  response.status(statusCode).send(body)
-})
+app.use('/api/transactions', transactionsRouter)
 
 app.listen(process.env.PORT, () =>
   console.log(`listening on ${process.env.PORT}`),
