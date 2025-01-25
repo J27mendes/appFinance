fd30672fd393d94f59c5ad3d03599bde88862a9b
J27mendez <mendes_j27@hotmail.com>
2025-01-25 11:10:19 -0300

    refactor: movie routes for app.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-import express from 'express'
 import 'dotenv/config.js'
-import { userRouter } from './src/routes/users.js'
-import { transactionsRouter } from './src/routes/transactions.js'
-
-const app = express()
-
-app.use(express.json())
-
-app.use('/api/users', userRouter)
-
-app.use('/api/transactions', transactionsRouter)
 
 app.listen(process.env.PORT, () =>
   console.log(`listening on ${process.env.PORT}`),
