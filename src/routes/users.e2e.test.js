1c317877d9264b1ab8ab2e81f10488a60ae4ac1b
J27mendez <mendes_j27@hotmail.com>
2025-01-27 18:42:29 -0300

    test: ensure UPDATE /api/users/:userId should return 404 if user not found

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -114,8 +114,16 @@ describe('User Routers E2E tests', () => {
     })
   })
 
-  it('Get /api/users/:userId should return 404 when user is not found', async () => {
+  it('GET /api/users/:userId should return 404 when user is not found', async () => {
     const response = await request(app).get(`/api/users/${faker.string.uuid()}`)
+
+    expect(response.status).toBe(404)
+  })
+
+  it('UPDATE /api/users/:userId should return 404 when user is not found', async () => {
+    const response = await request(app).patch(
+      `/api/users/${faker.string.uuid()}`,
+    )
 
     expect(response.status).toBe(404)
   })
