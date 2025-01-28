2269096c32f11f3222989557262d9ed1a80ca224
J27mendez <mendes_j27@hotmail.com>
2025-01-28 17:58:42 -0300

    refactor: removing try cathc in DeleteUserUseCase

--- src/useCases/user/deleteUserUseCase.js
+++ src/useCases/user/deleteUserUseCase.js
@@ -5,14 +5,11 @@ export class DeleteUserUseCase {
   }
 
   async execute(userId) {
-    try {
-      const deletedUser = await this.deleteUserRepository.execute(userId)
-      return deletedUser
-    } catch (error) {
-      if (error instanceof UserNotFoundError) {
-        throw error
-      }
-      throw new Error('Failed to delete user')
+    const deletedUser = await this.deleteUserRepository.execute(userId)
+
+    if (!deletedUser) {
+      throw new UserNotFoundError(userId)
     }
+    return deletedUser
   }
 }
