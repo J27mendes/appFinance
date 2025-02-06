82be96dc99945f8816962ae68a9a9dd7ab52ab1f
J27mendez <mendes_j27@hotmail.com>
2025-02-06 18:42:32 -0300

    feat: return webtoken created

--- src/useCases/user/loginUser.js
+++ src/useCases/user/loginUser.js
@@ -1,19 +1,31 @@
 import { InvalidPasswordError, UserNotFoundError } from '../../errors/index.js';
 
 export class LoginUserUseCase {
-  constructor(postgresCompareEmail, passwordComparatorAdapter) {
+  constructor(
+    postgresCompareEmail,
+    passwordComparatorAdapter,
+    tokensGeneratorAdapter,
+  ) {
     this.postgresCompareEmail = postgresCompareEmail;
     this.passwordComparatorAdapter = passwordComparatorAdapter;
+    this.tokensGeneratorAdapter = tokensGeneratorAdapter;
   }
   async execute(email, password) {
     const user = await this.postgresCompareEmail.execute(email);
     if (!user) {
       throw new UserNotFoundError();
     }
-    const passwordIsValid =
-      await this.passwordComparatorAdapter.execute(password);
+    const passwordIsValid = this.passwordComparatorAdapter.execute(
+      password,
+      user.password,
+    );
     if (!passwordIsValid) {
       throw new InvalidPasswordError();
     }
+
+    return {
+      ...user,
+      tokens: this.tokensGeneratorAdapter.execute(user.id),
+    };
   }
 }
