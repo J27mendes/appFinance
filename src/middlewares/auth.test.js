020f24d78187560956eba6c49c9b708d57928987
J27mendez <mendes_j27@hotmail.com>
2025-02-09 19:18:19 -0300

    test: ensure return 401 if jwt check is invalid

--- src/middlewares/auth.test.js
+++ src/middlewares/auth.test.js
@@ -1,5 +1,7 @@
 import { auth } from '../middlewares/auth.js';
+import jwt from 'jsonwebtoken';
 
+import.meta.jest.mock('jsonwebtoken');
 describe('auth middleares', () => {
   it('should return 401 if authorization token is missing', async () => {
     //arrange
@@ -16,6 +18,30 @@ describe('auth middleares', () => {
     await auth(request, response, next);
 
     //act & assert
+    expect(response.status).toHaveBeenCalledWith(401);
+    expect(response.send).toHaveBeenCalledWith({ message: 'Unauthorized' });
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  it('should return 401 if jwt check is invalid', async () => {
+    //arrange
+    const request = {
+      headers: { authorization: 'Bearer TokenInvalido' },
+    };
+
+    const response = {
+      status: import.meta.jest.fn().mockReturnThis(),
+      send: import.meta.jest.fn(),
+    };
+
+    //act
+    const next = import.meta.jest.fn();
+
+    import.meta.jest.spyOn(jwt, 'verify').mockImplementationOnce(null);
+
+    await auth(request, response, next);
+
+    // assert
     expect(response.status).toHaveBeenCalledWith(401);
     expect(response.send).toHaveBeenCalledWith({ message: 'Unauthorized' });
     expect(next).not.toHaveBeenCalled();
