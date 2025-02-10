38053ad70962736b2a2845a959b24ea4bba94627
J27mendez <mendes_j27@hotmail.com>
2025-02-10 20:36:34 -0300

    test: return a forbiddenError if transaction.id is not equal to params.id

--- src/useCases/transaction/updateTransactionUseCase.test.js
+++ src/useCases/transaction/updateTransactionUseCase.test.js
@@ -1,5 +1,6 @@
 import { UpdateTransactionUseCase } from './updateTransactionUseCase';
 import { transaction } from '../../tests/fixtures/index';
+import { ForbiddenError } from '../../errors';
 
 describe('UpdateTransactionUseCase', () => {
   const transactionId = transaction.id;
@@ -75,5 +76,24 @@ describe('UpdateTransactionUseCase', () => {
     expect(updateTransactionRepositorySpy).toHaveBeenCalledWith(transactionId, {
       amount: transaction.amount,
     });
+  });
+
+  it('should return a forbiddenError if transaction.id is not equal to params.id', async () => {
+    //arrange
+    const { sut, getTransactionById } = makeSut();
+    import.meta.jest
+      .spyOn(getTransactionById, 'execute')
+      .mockResolvedValueOnce({
+        id: 'some_transaction_id',
+        user_id: 'another_user_id',
+      });
+
+    const params = { userId: 'user_id_not_matching' };
+
+    // act
+    const promise = sut.execute('some_transaction_id', params);
+
+    // assert
+    await expect(promise).rejects.toThrow(ForbiddenError);
   });
 });
