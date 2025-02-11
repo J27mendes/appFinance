2e8ae149e3e0a493726bc5b2a4c255ad003a92f9
J27mendez <mendes_j27@hotmail.com>
2025-02-11 11:51:31 -0300

    test: ensure return 400 if refresh token is invalid

---
+++ src/controllers/user/refreshToken.test.js
@@ -0,0 +1,38 @@
+import { RefreshTokenController } from './refreshToken';
+
+describe('RefreshTokenController', () => {
+  class RefreshTokenUseCaseStub {
+    execute() {
+      return {
+        accessToken: 'valid_access_token',
+        refreshToken: 'valid_refresh_token',
+      };
+    }
+  }
+
+  const makeSut = () => {
+    const refreshTokenUseCase = new RefreshTokenUseCaseStub();
+    const sut = new RefreshTokenController(refreshTokenUseCase);
+
+    return {
+      sut,
+      refreshTokenUseCase,
+    };
+  };
+
+  it('should return 400 if refresh token is invalid', async () => {
+    //arrange
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        refreshToken: 2,
+      },
+    };
+
+    //act
+    const response = await sut.execute(httpRequest);
+
+    //assert
+    expect(response.statusCode).toBe(400);
+  });
+});
