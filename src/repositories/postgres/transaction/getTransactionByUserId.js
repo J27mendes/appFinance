9d91bb9b6b06cb6acc6d764687cad645b94fde66
J27mendez <mendes_j27@hotmail.com>
2025-02-17 09:55:42 -0300

    feat: add date gte & lte in PostgresTransactionByIdRepository

--- src/repositories/postgres/transaction/getTransactionByUserId.js
+++ src/repositories/postgres/transaction/getTransactionByUserId.js
@@ -1,9 +1,13 @@
 import { prisma } from '../../../../prisma/prisma.js';
 export class PostgresGetTransactionsByIdRepository {
-  async execute(userId) {
+  async execute(userId, from, to) {
     return await prisma.transaction.findMany({
       where: {
         user_id: userId,
+        date: {
+          gte: from ? new Date(from) : undefined,
+          lte: to ? new Date(to) : undefined,
+        },
       },
     });
   }
