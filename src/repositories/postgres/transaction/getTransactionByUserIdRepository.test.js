e16b5d0531c4dd8a70ff5c70e24f4d01bd8b95e9
J27mendez <mendes_j27@hotmail.com>
2025-02-17 09:59:08 -0300

    test: add variables from, to date in PostgresGetTransactionsByIdRepository

--- src/repositories/postgres/transaction/getTransactionByUserIdRepository.test.js
+++ src/repositories/postgres/transaction/getTransactionByUserIdRepository.test.js
@@ -4,16 +4,20 @@ import { transaction, user } from '../../../tests/fixtures';
 import { PostgresGetTransactionsByIdRepository } from './getTransactionByUserId';
 
 describe('PostgresGetTransactionsByIdRepository', () => {
+  const from = '2024-01-01';
+  const to = '2024-02-01';
+
   it('should get transaction by user id on db', async () => {
+    const date = '2024-01-03';
     //arrange
     const sut = new PostgresGetTransactionsByIdRepository();
     await prisma.user.create({ data: user });
     await prisma.transaction.create({
-      data: { ...transaction, user_id: user.id },
+      data: { ...transaction, date: new Date(date), user_id: user.id },
     });
 
     //act
-    const result = await sut.execute(user.id);
+    const result = await sut.execute(user.id, from, to);
 
     //assert
     expect(result.length).toBe(1);
@@ -21,11 +25,9 @@ describe('PostgresGetTransactionsByIdRepository', () => {
     expect(result[0].type).toBe(transaction.type);
     expect(result[0].user_id).toBe(user.id);
     expect(String(result[0].amount)).toBe(String(transaction.amount));
-    expect(dayjs(result[0].date).daysInMonth()).toBe(
-      dayjs(transaction.date).daysInMonth(),
-    );
-    expect(dayjs(result[0].date).month()).toBe(dayjs(transaction.date).month());
-    expect(dayjs(result[0].date).year()).toBe(dayjs(transaction.date).year());
+    expect(dayjs(result[0].date).daysInMonth()).toBe(dayjs(date).daysInMonth());
+    expect(dayjs(result[0].date).month()).toBe(dayjs(date).month());
+    expect(dayjs(result[0].date).year()).toBe(dayjs(date).year());
   });
 
   it('should call Prisma with correct params', async () => {
@@ -34,12 +36,16 @@ describe('PostgresGetTransactionsByIdRepository', () => {
     const prismaSpy = import.meta.jest.spyOn(prisma.transaction, 'findMany');
 
     //act
-    await sut.execute(user.id);
+    await sut.execute(user.id, from, to);
 
     //assert
     expect(prismaSpy).toHaveBeenCalledWith({
       where: {
         user_id: user.id,
+        date: {
+          gte: new Date('2024-01-01'), // Converte a string do teste para Date
+          lte: new Date('2024-02-01'),
+        },
       },
     });
   });
